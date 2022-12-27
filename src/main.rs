ab47afe35461d8c0872af23897261b7144c19cf2
filip_trojan <Tarantula2018>
2022-12-27 19:52:38 +0100

    refactor better

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,14 @@
 /// Make HTTP request to Spotify API
 /// https://blog.logrocket.com/making-http-requests-rust-reqwest/
 /// get token: https://developer.spotify.com/console/get-search-item/?q=Muse&type=track&market=US&limit=5&offset=5&include_external=
-use reqwest;
-use reqwest::header::{AUTHORIZATION, CONTENT_TYPE, ACCEPT};
-use rust_spotify::data;
+use rust_spotify::spotify::data;
+use rust_spotify::spotify;
 
-#[tokio::main]
-async fn main() {
-    let client = reqwest::Client::new();
-    let url = format!(
-        "https://api.spotify.com/v1/search?q={query}&type=track,artist",
-        // go check out her latest album. It's 🔥
-        query = "Little Simz"
-    );
-    let response = client
-        .get(url)
-        .header(AUTHORIZATION, "Bearer BQCUo4fjU0yU2BvJWdaNqay0FRqoTtgiRHQ0zfLsEi6-F4NFywQLNF_soooazl7erMaDmr6FXEMPCEgMB8Fvu7SXop10MpCTW6bNzR7NwCN2xBRtKcnBkX6aIUDYtm8X9azgqvqh4y60ZOcSs_8hoH17GtHUq4tPp283x9TzWpVznWpNZC0tE4cNuax9SncXYwQ")
-        .header(CONTENT_TYPE, "application/json")
-        .header(ACCEPT, "application/json")
-        .send()
-        .await
-        .unwrap();
-    match response.status() {
-        reqwest::StatusCode::OK => {
-            match response.json::<data::APIResponse>().await {
-                Ok(parsed) => data::print_tracks(parsed.tracks.items.iter().collect()),
-                Err(_) => println!("Hm, the response did not match the shape we expected."),
-            };
-        },
-        reqwest::StatusCode::UNAUTHORIZED => {
-            println!("Need to grab a new token at https://developer.spotify.com/console/get-search-item/?q=Muse&type=track&market=US&limit=5&offset=5&include_external=");
-        },
-        _ => {
-            panic!("Uh oh! Something unexpected happened.");
-        },
-    };
+fn main() {
+    let s = spotify::Spotify::create();
+    let tracks = s.search_for_tracks("Little Simz");
+    match tracks {
+        None => {},
+        Some(parsed) => data::print_tracks(parsed.tracks.items.iter().collect()),
+    }
 }
---
+++ src/spotify.rs
@@ -0,0 +1,67 @@
+use reqwest;
+use reqwest::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE};
+
+pub mod data;
+
+
+pub fn url_for_query(query: &str) -> String {
+    let result = format!(
+        "https://api.spotify.com/v1/search?q={query}&type=track,artist",
+        query = query
+    );
+    result
+}
+
+
+pub struct Spotify {
+    client: reqwest::Client,
+    auth_token: String,
+}
+
+impl Spotify {
+
+    pub fn create() -> Self {
+        let token = "BQC4CYMB5rHeM27LtQd5u1lOv5btNiSSmDVtC8Mqd_6LPHvWkVrzJb7smjJHX1F-ojatcjfpa1JLaz0wipXIiTQ1KCWaGb__yrQ7jU-ql3g2n7KQSHkgCEnGa7ze5_86B_lBUEzxjUpPUMSi8yBni2tPZ7es0kU-eGlmb7lT9ILZ30fjlmaxKyS8TPE7dABnf_c";
+        let instance = Spotify {
+            client: reqwest::Client::new(),
+            auth_token: token.parse().unwrap(),
+        };
+        instance
+    }
+
+    #[tokio::main]
+    pub async fn search_for_tracks(self, query: &str) -> Option<data::APIResponse> {
+        let url = url_for_query(query);
+        let auth = format!("Bearer {}", self.auth_token);
+        let response = self.client
+            .get(url)
+            .header(AUTHORIZATION, auth)
+            .header(CONTENT_TYPE, "application/json")
+            .header(ACCEPT, "application/json")
+            .send()
+            .await
+            .unwrap();
+        let response_valid = match response.status() {
+            reqwest::StatusCode::OK => Some(response),
+            reqwest::StatusCode::UNAUTHORIZED => {
+                println!("Need to grab a new token at https://developer.spotify.com/console/get-search-item/?q=Muse&type=track&market=US&limit=5&offset=5&include_external=");
+                None
+            },
+            _ => {
+                println!("Uh oh! Something unexpected happened.");
+                None
+            },
+        };
+        let result = match response_valid {
+            None => None,
+            Some(r) => match r.json::<data::APIResponse>().await {
+                Ok(parsed) => Some(parsed),
+                Err(_) => {
+                    println!("Hm, the response did not match the shape we expected.");
+                    None
+                }
+            }
+        };
+        result
+    }
+}
